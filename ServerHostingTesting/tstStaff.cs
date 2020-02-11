fc4ba892b2fca5378481112070c2bde98f88bf64
p2507766 <p2507766@my365.dmu.ac.uk>
2020-02-11 16:13:59 +0000

    added the testing and class for staff management

---
+++ ServerHostingTesting/tstStaff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ServerHostingTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServerHostingTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Created an instance of staff
+            clsStaff newStaff = new clsStaff();
+            //this is to test that it exists
+            Assert.IsNotNull(newStaff);
+
+        }
+
+        [TestMethod]
+        public void StaffNoExists()
+        {
+            clsStaff newStaff = new clsStaff();
+
+        }
+    }
+}
