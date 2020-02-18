7d7d74a4d002826f1dc173392e8fd5d68d0570c9
p2423644 <p2423644@GWHXX681WIN016W>
2020-02-18 16:29:46 +0000

    added more tests

--- ServerHostingTesting/tstCustomer.cs
+++ ServerHostingTesting/tstCustomer.cs
@@ -5,7 +5,7 @@ using ServerHostingTesting;
 namespace Test_Framework
 {
     [TestClass]
-    public class tstCustomer
+    public class clsCustomer
     {
         [TestMethod]
         public void InstanceOK()
@@ -14,6 +14,28 @@ namespace Test_Framework
             clsCustomer AnCustomer = new clsCustomer();
             //test to see that it exists
             Assert.IsNotNull(AnCustomer);
+
+        }
+        [TestMethod]
+        public void DateAddedOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnCustomer.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.DateAdded, TestData);
+        }
+        public void PasswordOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            String TestData = "Samj1046";
+            AnCustomer.Password = TestData;
+            Assert.AreEqual(AnCustomer.Password, TestData);
+
         }
     }
 }
