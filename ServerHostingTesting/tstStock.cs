63b36bf7b21c0dafa6f018fd65cc1e8b4a8fd5ea
Robert Hope <p2510235@mydmu.ac.uk>
2020-02-18 15:37:52 +0000

    Added testing and class for stock management

---
+++ ServerHostingTesting/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerHostingLibrary;
+
+namespace ServerHostingTesting
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AvailableStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AvailableStock);
+        }
+    }
+}
