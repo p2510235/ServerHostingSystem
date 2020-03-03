83ee220efaa1014e43c01bcc294c4dd293dc609e
p16175501 <p16175501@my365.dmu.ac.uk>
2020-03-03 16:50:31 +0000

    commit

--- ServerHostingTesting/tstOrder.cs
+++ ServerHostingTesting/tstOrder.cs
@@ -15,5 +15,19 @@ namespace ServerHostingTesting
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void AddressNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnAddress = new clsOrder();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnAddress.OrderNo= TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.OrderNo, TestData);
+        }
+
     }
 }
