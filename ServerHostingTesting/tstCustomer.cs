2bed8d2e01a2623c48d1eb9eb0199ece41f24606
salimah <salimah.mohamed@dmu.ac.uk>
2020-03-26 15:04:14 +0000

    fixed properties duplication error. However you need to take a look at your test class and refactor it completely. It  is very messy and there a lot of syntax errors atm. Do this before you continue with your find method.

--- ServerHostingTesting/tstCustomer.cs
+++ ServerHostingTesting/tstCustomer.cs
@@ -11,6 +11,7 @@ namespace Test_Framework
         string PhoneNumber = "07946274531";
         string DateAdded = DateTime.Now.Date.ToString();
         string PostCode = "Ng31 8RL";
+
         [TestMethod]
         public void InstanceOK()
         {
@@ -20,139 +21,139 @@ namespace Test_Framework
             Assert.IsNotNull(AnCustomer);
 
         }
-        [TestMethod]
-        public void DateAddedOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            //create some test data to assign to the property
-            DateTime TestData = DateTime.Now.Date;
-            //assign the data to the property
-            AnCustomer.DateAdded = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(AnCustomer.DateAdded, TestData);
-        }
-        public void PasswordOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            String TestData = "Samj1046";
-            AnCustomer.Password = TestData;
-            Assert.AreEqual(AnCustomer.Password, TestData);
+     [TestMethod]
+         public void DateAddedOK()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             //create some test data to assign to the property
+             DateTime TestData = DateTime.Now.Date;
+             //assign the data to the property
+             AnCustomer.DateAdded = TestData;
+             //test to see that the two values are the same
+             Assert.AreEqual(AnCustomer.DateAdded, TestData);
+         }
+         public void PasswordOK()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             String TestData = "Samj1046";
+             AnCustomer.Password = TestData;
+             Assert.AreEqual(AnCustomer.Password, TestData);
 
-        }
-        [TestMethod]
-        public void FindMethod()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Int32 CustomerNo = 1;
-            Found = AnCustomer.Find(CustomerNo);
-            Assert.IsTrue(Found);
-        }
-        [TestMethod]
-        public void TestCustomerNoFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Boolean OK = true;
-            Int32 CustomerNo = 21;
-            Found = AnCustomer.Find(CustomerNo);
-            if (AnCustomer.CustomerNo != 21)
-            {
-                OK = false;
-            }
-            Assert.IsTrue(Found);
-        }
-        [TestMethod]
-        public void TestDateAddedFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Boolean OK = true;
-            Int32 CustomerNo = 21;
-            Found = AnCustomer.Find(CustomerNo);
-            if (AnCustomer.DateAdded != Convert.ToDateTime("16/09/2015"))
-            {
-                OK = false;
-            }
-            Assert.IsTrue(OK);
-        }
-        [TestMethod]
-        public void TestPhoneAddedFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Boolean OK = true;
-            Int32 CustomerNo = 21;
-            Found = AnCustomer.Find(CustomerNo);
-            if (AnCustomer.PhoneNumber != "07946274531")
-            {
-                OK = false;
-            }
-            Assert.IsTrue(OK);
-        }
-        [TestMethod]
-        public void TestNameFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Boolean OK = true;
-            Int32 CustomerNo = 21;
-            Found = AnCustomer.Find(CustomerNo);
-            if (AnCustomer.Name != "Sam")
-            {
-                OK = false;
-            }
-            Assert.IsTrue(Found);
-        }
-        [TestMethod]
-        public void TestPostCodeFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            Boolean found = false;
-            Boolean OK = true;
-            Int32 CustomerNo = 21;
-            Found = AnCustomer.Find(CustomerNo);
-            if (AnCustomer.Name != "NG31 8RL")
-            {
-                OK = false;
-            }
-            Assert.IsTrue(Found);
-        }
+         }
+         [TestMethod]
+         public void FindMethod()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Int32 CustomerNo = 1;
+             Found = AnCustomer.Find(CustomerNo);
+             Assert.IsTrue(Found);
+         }
+         [TestMethod]
+         public void TestCustomerNoFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Boolean OK = true;
+             Int32 CustomerNo = 21;
+             Found = AnCustomer.Find(CustomerNo);
+             if (AnCustomer.CustomerNo != 21)
+             {
+                 OK = false;
+             }
+             Assert.IsTrue(Found);
+         }
+         [TestMethod]
+         public void TestDateAddedFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Boolean OK = true;
+             Int32 CustomerNo = 21;
+             Found = AnCustomer.Find(CustomerNo);
+             if (AnCustomer.DateAdded != Convert.ToDateTime("16/09/2015"))
+             {
+                 OK = false;
+             }
+             Assert.IsTrue(OK);
+         }
+         [TestMethod]
+         public void TestPhoneAddedFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Boolean OK = true;
+             Int32 CustomerNo = 21;
+             Found = AnCustomer.Find(CustomerNo);
+             if (AnCustomer.PhoneNumber != "07946274531")
+             {
+                 OK = false;
+             }
+             Assert.IsTrue(OK);
+         }
+         [TestMethod]
+         public void TestNameFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Boolean OK = true;
+             Int32 CustomerNo = 21;
+             Found = AnCustomer.Find(CustomerNo);
+             if (AnCustomer.Name != "Sam")
+             {
+                 OK = false;
+             }
+             Assert.IsTrue(Found);
+         }
+         [TestMethod]
+         public void TestPostCodeFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             Boolean found = false;
+             Boolean OK = true;
+             Int32 CustomerNo = 21;
+             Found = AnCustomer.Find(CustomerNo);
+             if (AnCustomer.Name != "NG31 8RL")
+             {
+                 OK = false;
+             }
+             Assert.IsTrue(Found);
+         }
 
-        public void TestOwnServerFound()
-        {
-            //create an instance of the class we want to create
-            clsCustomer AnCustomer = new clsCustomer();
-            //boolean variable to store the result of the search
-            Boolean Found = false;
-            //boolean variable to record if data is OK (assume it is)
-            Boolean OK = true;
-            //create some test data to use with the method
-            Int32 AddressNo = 21;
-            //invoke the method
-            Found = AnCustomer.Find(AddressNo);
-            //check the property
-            if (AnCustomer.Active != true)
-            {
-                OK = false;
-            }
-            //test to see that the result is correct
-            Assert.IsTrue(OK);
-        }
-        public void ValidMethodOK()
-        {
-            clsCustomer AnCustomer = new clsCustomer();
-            String Error = "";
-            Error = AnCustomer.Valid(CusomterNo, DateAdded, Name, PostCode, PhoneNumber);
-            Assert.AreEqual(Error, "");
-        }
+         public void TestOwnServerFound()
+         {
+             //create an instance of the class we want to create
+             clsCustomer AnCustomer = new clsCustomer();
+             //boolean variable to store the result of the search
+             Boolean Found = false;
+             //boolean variable to record if data is OK (assume it is)
+             Boolean OK = true;
+             //create some test data to use with the method
+             Int32 AddressNo = 21;
+             //invoke the method
+             Found = AnCustomer.Find(AddressNo);
+             //check the property
+             if (AnCustomer.Active != true)
+             {
+                 OK = false;
+             }
+             //test to see that the result is correct
+             Assert.IsTrue(OK);
+         }
+         public void ValidMethodOK()
+         {
+             clsCustomer AnCustomer = new clsCustomer();
+             String Error = "";
+             Error = AnCustomer.Valid(CusomterNo, DateAdded, Name, PostCode, PhoneNumber);
+             Assert.AreEqual(Error, "");
+         }
     }
 }
