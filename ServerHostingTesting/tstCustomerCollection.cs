ba30b8bb44cbfb36eef5ae6cbf845b0bf05933e4
salimah <salimah.mohamed@dmu.ac.uk>
2020-03-30 20:56:02 +0100

    syntax errors in tstCustomercollection issues fixed

--- ServerHostingTesting/tstCustomerCollection.cs
+++ ServerHostingTesting/tstCustomerCollection.cs
@@ -6,32 +6,36 @@ using ServerHostingTesting;
 
 namespace ServerHostingTesting
 {
-    
+
     [TestClass]
-  
-    
+    public class tstCustomerCollection
+    {
+
+
         [TestMethod]
         public void InstanceOK()
         {
             clsCustomerCollection AllCustomers = new clsCustomerCollection();
             Assert.IsNotNull(AllCustomers);
         }
+
+      
         public void NameListOK()
         {
             clsCustomerCollection AllCustomers = new clsCustomerCollection();
-            List<clsCustomer> TestList = new List<ClsCustomer>();
-        clsCustomer TestItem = new clsCustomer();
-        TestItem.Active = true;
-        TestItem.CustomerNo = 1;
-        TestItem.Name = "Sam";
-        TestItem.DateAdded = DateTime.Now.Date;
-        TestItem.PhoneNumber = "07946274531";
-        TestItem.PostCode = "Ng31 8RL";
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.CustomerNo = 1;
+            TestItem.Name = "Sam";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.PhoneNumber = "07946274531";
+            TestItem.PostCode = "Ng31 8RL";
 
-        TestList.Add(TestItem);
-        AllCustomers.CustomerList = TestList;
-        Assert.AreEqual(AllCustomers.CustomerList, TestList);
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
 
         }
     }
-
+}
